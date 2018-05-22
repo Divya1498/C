a0915c94d59d0340e6b21800b9cda899b5dea167
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-05-22 11:45:34 +0530

    Create even.c

---
+++ codekatta/even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+
+int main(void) {
+	// your code goes h
+int i ,a,b;
+scanf("%d%d",&a,&b);
+for(i=a+1;i<b;i++)
+
+{
+	if(i%2==0)
+	printf("%d",i);
+}
+return 0;
+}
