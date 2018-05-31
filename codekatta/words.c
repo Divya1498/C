1706cdd5c779a633c734afd30366b22ab9f82098
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-05-31 21:42:52 +0530

    Create words.c

---
+++ codekatta/words.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(void){int a;
+            printf("enter the no 1 to 10");
+	scanf("%da",&a);
+switch(a)
+{
+	case 1:
+	       printf("\n one");
+	       break;
+	case 2:printf("\n two");
+	       break;
+	case 3:printf("\n three");
+	       break;
+	case 4:printf("\n four");
+	       break;
+	case 5:printf("\n five");
+	       break;
+	case 6:printf("\n six");
+	       break;
+	case 7:printf("\n seven");
+	       break;
+	case 8:printf("\n eight");
+	       break;
+	case 9:printf("\n nine");
+	       break;
+	case 10:printf("\n ten");
+	        break;
+}
+return 0;		
+}
